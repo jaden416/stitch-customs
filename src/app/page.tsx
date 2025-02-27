fa8735accc8d8e2ed2f895fca94121fe259e3521
Jaden Lewis-Chambers <you@example.com>
2025-02-26 21:05:15 -0500

    grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
-import Item from "./components/Item";
-
 export default function Home() {
   return (
-    <div className="my-[7rem] flex h-full w-full items-center justify-center">
-      <div className="h-min w-full max-w-[1100px]">
-        <div className="content flex h-[700px] gap-[1.6rem]">
-          <div className="column flex h-full flex-1 flex-col gap-[1.6rem]">
-            <Item className="flex-grow-[2]"></Item>
-            <Item className="flex-grow-[1]"></Item>
-          </div>
-          <div className="column flex h-full flex-1 flex-col gap-[1.6rem]">
-            <Item className="flex-grow-[1.16]"></Item>
-            <div className="flex flex-grow-[2] flex-col gap-[1.6rem]">
-              <Item className="flex-grow-[1]"></Item>
-              <div className="flex flex-grow-[1.17] flex-col gap-[1.6rem]">
-                <Item className="flex-grow-[1]"></Item>
-                <Item className="flex-grow-[1]"></Item>
-              </div>
-            </div>
-          </div>
+    <div className="relative">
+      <div className="relative mx-auto w-full bg-slate-500 px-6 xl:max-w-[120rem]">
+        <div className="grid w-full max-w-[120rem] grid-rows-2">
+          <div className="bg-slate-700"></div>
+          <div className="bg-slate-700"></div>
+          <div className="bg-slate-700"></div>
+          <div className="bg-slate-700"></div>
         </div>
       </div>
     </div>
